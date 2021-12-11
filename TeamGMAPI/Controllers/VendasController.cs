8ac6d75cb05cba3610155bce7ae449ead80826a8
Vitor Martins <vitormartins.one@gmail.com>
2021-12-11 11:11:29 -0300

    Update VendasController.cs

--- TeamGMAPI/Controllers/VendasController.cs
+++ TeamGMAPI/Controllers/VendasController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Base.BUSINESS.Interfaces;
+using Base.DOMAIN.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TeamGMAPI.Controllers;
 
 namespace BaseAPI.Controllers 
 {
@@ -6,5 +10,17 @@ namespace BaseAPI.Controllers
     [Route("[controller]")]
     public class VendasController : Controller
     {
+        [HttpPost]
+        [Route("InsereVenda")]
+        public async Task<IActionResult> InsereVenda(Venda venda, [FromServices]IVendaBusiness vendaBusiness)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+            else
+            {
+                var vendaSalva = await vendaBusiness.AdicionarVenda(venda);
+                return Ok(vendaSalva);
+            }
+        }
     }
 }
