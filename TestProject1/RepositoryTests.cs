40d992585d73272eb2d6cef6777ea5fdaab3cf7b
desafioatos2021 <desafioatos2021@gmail.com>
2021-12-16 23:54:28 -0400

    Inserção testes

---
+++ TestProject1/RepositoryTests.cs
@@ -0,0 +1,32 @@
+﻿
+using Base.DOMAIN.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using TeamGM.DATA.Context;
+
+namespace BaseAPI.Data.Tests
+{
+    [TestClass]
+    public class RepositoryTests
+    {
+
+        
+
+        
+
+        [TestMethod]
+        public void Test_Insert_Cliente()
+        {
+            Cliente clienteAtual = new Cliente() { IdCliente = 0, IdEndereco = 3, Nome = "Leonardo" };
+
+            Assert.IsTrue(!string.IsNullOrEmpty(clienteAtual.Nome) && clienteAtual.IdEndereco != 0);
+
+        }
+
+        
+
+
+    }
+}
